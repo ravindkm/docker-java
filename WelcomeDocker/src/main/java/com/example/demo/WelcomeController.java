a5bf7632f43a273b102e77e57953e18b14ec4f9e
ravinder.kumar <ravinder.kumar@IRIS-CSG-2136.irissoftware.com>
2020-03-22 11:23:05 +0530

    Initial Commit.

---
+++ WelcomeDocker/src/main/java/com/example/demo/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+	@GetMapping("/welcome")
+	public String helloWorld() {
+		return "Welcome!! Docker world";
+	}
+}
