12a383a68af1b015cd8f9b3480edfb73e3b7c057
ravindkm 1ravinder1@gmail.com <ravinder.kumar@IRIS-CSG-2136.irissoftware.com>
2020-04-28 12:39:58 +0530

    Include servlet configuration

--- CabSystem/src/main/java/com/trariti/app/CabSystemApplication.java
+++ CabSystem/src/main/java/com/trariti/app/CabSystemApplication.java
@@ -2,10 +2,17 @@ package com.trariti.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CabSystemApplication {
+public class CabSystemApplication extends SpringBootServletInitializer {
 
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(CabSystemApplication.class);
+	  }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CabSystemApplication.class, args);
 	}
